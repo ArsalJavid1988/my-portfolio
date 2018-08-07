31911b45447a77e71d2a2bd95365084df5698743
Arsal Javid <Arsal.javid@gmail.com>
2018-08-07 17:41:50 +0100

    Updated Lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -32,7 +32,7 @@ def lambda_handler(event, context):
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
                 portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+                ExtraArgs={'ContentType':'basestring'})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print "Job Done"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio Deployed Successfully")
