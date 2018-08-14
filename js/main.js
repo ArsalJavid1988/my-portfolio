67dd1256f705d385cddbf1155cbdf7c1611122ac
Arsal Javid <Arsal.javid@gmail.com>
2018-08-14 20:09:41 +0100

    Removed third bubble

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ const myWork = [
       'src': "images/example2.png",
       'comment': ""
     }
-  },
-  {
+  }
+/*  {
     'title': "Work Example",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
@@ -33,7 +33,7 @@ const myWork = [
       'comment': `"Bengal cat” by roberto shabs is licensed under CC BY 2.0
                     https://www.flickr.com/photos/37287295@N00/2540855181"`
     }
-  }
+  }*/
 ]
 
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
