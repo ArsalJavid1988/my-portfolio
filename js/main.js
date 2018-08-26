0d151db78689c22b34b20713696ff7ac0dac5934
Javid <arsal.javid@atos.net>
2018-08-26 11:06:44 +0100

    Updated Slideshow - Text now visible above the slide

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 
+
 const myWork = [
   {
     'title': "My Portfolio Website",
@@ -34,6 +35,8 @@ const myWork = [
                     https://www.flickr.com/photos/37287295@N00/2540855181"`
     }
   }*/
+
 ]
+
 
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
