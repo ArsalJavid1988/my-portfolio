a034f8b3457d86c8a64e581e575645a5641ed967
Javid <arsal.javid@atos.net>
2018-08-26 15:22:49 +0100

    Added React Slideshow - TEST

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
 import SlideShow from './mySlide';
+import 'react-animated-slider/build/horizontal.css';
 
 
 const myWork = [
