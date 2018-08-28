a4816c1f9dc4a2de37c1b7b32fb48d3e10fd461d
Javid <arsal.javid@atos.net>
2018-08-28 20:04:27 +0100

    Integrated React Animated Slider in to demo2.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ExampleWork from './example-work';
-import SlideShow from './mySlide';
+//import SlideShow from './mySlide';
+import MySlidesShow from './mySlide';
 
 const myWork = [
   {
@@ -37,37 +38,9 @@ const myWork = [
   }*/
 
 ]
-const content = [
-	{
-		title: 'Vulputate Mollis Ultricies Fermentum Parturient',
-		description:
-		'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
-		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Luan Gjokaj',
-		userProfile: 'https://i.imgur.com/JSW6mEk.png'
-	},
-	{
-		title: 'Tortor Dapibus Commodo Aenean Quam',
-		description:
-		'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.',
-		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'Erich Behrens',
-		userProfile: 'https://i.imgur.com/0Clfnu7.png'
-	},
-	{
-		title: 'Phasellus volutpat metus',
-		description:
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Bruno Vizovskyy',
-		userProfile: 'https://i.imgur.com/4KeKvtH.png'
-	}
-];
 
 
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
 //ReactDOM.render(<SlideShow/>, document.getElementById('SlideShow'));
-ReactDOM.render(<SlideShow />, document.getElementById('mySlide'));
+//ReactDOM.render(<SlideShow />, document.getElementById('mySlide'));
+ReactDOM.render(<MySlidesShow />,document.getElementById('MySlidesShow'));
--- js/mySlide.js
+++ js/mySlide.js
@@ -1,4 +1,37 @@
 import React from 'react';
+import Slider from 'react-animated-slider';
+require('react-animated-slider/build/horizontal.css');
+require('./slider-animations.css');
+require('./styles.css');
+const content = [
+	{
+		title: 'This is my first Slide',
+		description:
+		'Let us see if this works',
+	//	button: 'Read More',
+		image: '../images/islamabad.jpg',
+		user: 'Arsal Javid',
+		userProfile: '../images/test.png'
+	},
+	{
+		title: 'This is my second Slide',
+		description:
+		'Time to move',
+	//	button: 'Discover',
+		image: '..//images/example2.png',
+		user: 'Arsal Javid',
+		userProfile: '../images/test.png'
+	},
+	{
+		title: 'Slide Number 3 coming right up',
+		description:
+		'Done and Dusted',
+	//	button: 'Buy now',
+		image: '../images/example5.png',
+		user: 'Arsal Javid',
+		userProfile: '../images/test.png'
+	}
+];
 
 class SlideShow extends React.Component {
   render(){
@@ -10,7 +43,37 @@ class SlideShow extends React.Component {
          </h2>
          </section>
 </span>
-)
+)}}
+class MySlidesShow extends React.Component {
+  render(){
+    return(
+      <span>
+
+      <Slider className="slider-wrapper">
+      {content.map((item, index) => (
+        <div
+          key={index}
+          className="slider-content"
+          style={{ background: `url('${item.image}') no-repeat center center` }}
+        >
+          <div className="inner">
+            <h1>{item.title}</h1>
+            <p>{item.description}</p>
+            </div>
+          <section>
+            <img src={item.userProfile} alt={item.user} />
+            <span>
+              Posted by <strong>{item.user}</strong>
+            </span>
+          </section>
+        </div>
+      ))}
+      </Slider>
+
+
+</span>
+    )
+  }
 }
-}
-export default SlideShow;
+//export default SlideShow;
+export default MySlidesShow;
